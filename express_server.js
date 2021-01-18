3fc5690ec5f2b15ad482daa3d0a2b9bed4b3ee2d
Jacob Hein <jacobhein1892@gmail.com>
2021-01-18 22:35:48 +0000

    added a new route that included long and short link

--- express_server.js
+++ express_server.js
@@ -22,6 +22,11 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase}
   res.render('urls_index', templateVars);
 });
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render('urls_show', templateVars);
+})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
