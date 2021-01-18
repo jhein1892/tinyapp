7157525ca75441282a8ff909bb863c3e7aaaf941
Jacob Hein <jacobhein1892@gmail.com>
2021-01-18 20:54:53 +0000

    json string representing urldatabase

--- express_server.js
+++ express_server.js
@@ -10,6 +10,9 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
