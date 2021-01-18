6c390cc8f2daca2362638d23ad9129152fb6d7ca
Jacob Hein <jacobhein1892@gmail.com>
2021-01-18 23:21:54 +0000

    added random generator, as well as form

--- express_server.js
+++ express_server.js
@@ -3,11 +3,17 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString(){
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -29,6 +35,10 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 })
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 
 app.listen(PORT, () => {
