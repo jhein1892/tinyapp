c44ecf6bacf21b9c685c0dcf5d49d50a74e6038c
Jacob Hein <jacobhein1892@gmail.com>
2021-01-18 23:04:35 +0000

    added a form for new urls

--- express_server.js
+++ express_server.js
@@ -22,6 +22,9 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase}
   res.render('urls_index', templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
