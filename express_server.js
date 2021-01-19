c12a7a2b5ef58fb9a300cc0632031ebaff3edb73
Jacob Hein <jacobhein1892@gmail.com>
2021-01-19 22:36:21 +0000

    added login/logout buttons

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser')
 
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
@@ -16,7 +17,7 @@ const urlDatabase = {
 function generateRandomString() {
   return Math.floor((1 + Math.random()) * 0x100000).toString(16);
 }
-
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -27,11 +28,40 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
-  const templateVars = {
-    urls: urlDatabase
-  }
-  res.render('urls_index', templateVars);
+  let username = req.cookies['username']
+
+    const templateVars = {
+      username,
+      urls: urlDatabase
+    }
+    res.render('urls_index', templateVars);
+
 });
+// app.get("/login", (req, res) => {
+//   res.render('login')
+// })
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username)
+  let username = req.body.username
+  
+      const templateVars = {
+      username,
+      urls: urlDatabase
+    }
+    res.render('urls_index', templateVars);
+
+}) 
+app.post('/logout', (req,res) => {
+  res.clearCookie('username')
+  res.cookie('username', req.body.username)
+  let username = req.body.username
+  delete req.cookies['username'];
+  const templateVars = {
+    username,
+    urls: urlDatabase
+  };
+  res.render('urls_index', templateVars )
+})
 app.get("/u/:shortURL", (req, res) => {
   res.redirect(urlDatabase[req.params.shortURL])
 })
@@ -40,6 +70,7 @@ app.get("/urls/new", (req, res) => {
 });
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
+    username: req.cookies["username"],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
