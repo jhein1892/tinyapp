23ad641859238a215e3971da1b1d241ffafb723b
Jacob Hein <jacobhein1892@gmail.com>
2021-01-20 01:28:52 +0000

    tied in the registration POST

--- express_server.js
+++ express_server.js
@@ -1,23 +1,42 @@
+// set up the server
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require('cookie-parser')
-
 app.set("view engine", "ejs");
+
+//setting up cokoie parsing
+const cookieParser = require('cookie-parser')
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+// Objects that are used
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+// Helper Functions
 function generateRandomString() {
   return Math.floor((1 + Math.random()) * 0x100000).toString(16);
 }
 app.use(cookieParser());
+
+// Creating endpoints
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -37,11 +56,19 @@ app.get("/urls", (req, res) => {
     res.render('urls_index', templateVars);
 
 });
-// app.get("/login", (req, res) => {
-//   res.render('login')
-// })
 app.get('/register', (req, res) => {
   res.render('register')
+})
+app.post('/register', (req, res) => {
+  let id = generateRandomString()
+  users[id] = {
+    id: id,
+    email: req.body.email,
+    password: req.body.password
+  }
+  res.cookie("user_id", id)
+  res.redirect('/urls');
+  console.log(users)
 })
 app.post("/login", (req, res) => {
   res.cookie('username', req.body.username)
@@ -110,4 +137,4 @@ app.post("/urls", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-}); 
+});  
